ea3ef474ba770a356314f7540e6f2b8a524d4989
Tim Hobbs <jesus.tesh@gmail.com>
2025-05-10 01:36:46 -0700

    Add .env to .gitignore

--- scripts/install-age.sh
+++ scripts/install-age.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 AGE_LATEST_VERSION=$(curl -s "https://api.github.com/repos/FiloSottile/age/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-curl -Lo age.tar.gz "https://github.com/FiloSottile/age/releases/latest/download/age-v${AGE_LATEST_VERSION}-linux-arm64.tar.gz"
+curl -Lo age.tar.gz "https://github.com/FiloSottile/age/releases/latest/download/age-v${AGE_LATEST_VERSION}-linux-amd64.tar.gz"
 tar -xzf age.tar.gz
 sudo mv age/age /usr/local/bin
 sudo mv age/age-keygen /usr/local/bin
--- scripts/install-sops.sh
+++ scripts/install-sops.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 SOPS_LATEST_VERSION=$(curl -s "https://api.github.com/repos/getsops/sops/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-curl -Lo sops.deb "https://github.com/getsops/sops/releases/download/v${SOPS_LATEST_VERSION}/sops_${SOPS_LATEST_VERSION}_arm64.deb"
+curl -Lo sops.deb "https://github.com/getsops/sops/releases/download/v${SOPS_LATEST_VERSION}/sops_${SOPS_LATEST_VERSION}_amd64.deb"
 sudo apt --fix-broken install ./sops.deb
 rm -rf sops.deb
